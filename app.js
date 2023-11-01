c0f350be2310e8d6417ff3b66c6e5939c893dd7e
aldii <200401141@student.umri.ac.id>
2023-11-01 16:50:45 +0700

    feat: sc cd

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ const {PORT = 3000} = process.env
 app.use(express.json())
 app.use(morgan('dev'))
 
+app.get('/', (req, res)=>{
+    return res.json({
+        status: true,
+        message: 'hii develop',
+        err:null,
+        data: null
+    })
+})
+
 const user = require('./routes/user.routes')
 const profile = require('./routes/profile.routes')
 
